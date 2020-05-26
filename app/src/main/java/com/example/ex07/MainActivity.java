16d53f3741b562a50882eb5ebdd8bc79363fc3c7
Shahar Yani <sy4172@bs.amalnet.k12.il>
2020-05-26 20:40:36 +0300

    Update: the xml is ready Try to build the code

--- app/src/main/java/com/example/ex07/MainActivity.java
+++ app/src/main/java/com/example/ex07/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.ex07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    RadioButton rB1, rB2,rB3;
+    RadioButton rB1, rB2, rB3;
     Switch switcH;
+    LinearLayout screen;
+    RadioGroup rG;
+    int celetedId;
 
 
     @Override
@@ -20,10 +28,41 @@ public class MainActivity extends AppCompatActivity {
         rB2 = findViewById(R.id.rB2);
         rB3 = findViewById(R.id.rB3);
         switcH = findViewById(R.id.switcH);
-
-        if (switcH.isChecked()){
-
-        }
-
+        screen = findViewById(R.id.screen);
     }
+
+    public void change(View view) {
+        if (!(switcH.isChecked())){
+            celetedId = rG.getCheckedRadioButtonId();
+            switch (celetedId){
+                case R.id.rB1:{
+                    screen.setBackgroundColor(Color.GREEN);
+                    rG.clearCheck();
+                }
+                break;
+
+                case R.id.rB2:{
+                    screen.setBackgroundColor(Color.BLUE);
+                    rG.clearCheck();
+                }
+                break;
+
+                case R.id.rB3:{
+                    screen.setBackgroundColor(Color.YELLOW);
+                    rG.clearCheck();
+                }
+                break;
+
+                case R.id.rB4:{
+                    screen.setBackgroundColor(Color.RED);
+                    rG.clearCheck();
+                }
+                break;
+
+                default:{
+                }
+            }
+        }
+    }
+
 }
